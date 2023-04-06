1f96b37ee2f7837dc854f44f7fca4bbbc5e6fdf7
Gede Sugandi <gedesugandi@gmail.com>
2023-04-06 17:20:53 +0800

    update config 2

--- configuration.php
+++ configuration.php
@@ -5,13 +5,19 @@ class SConfig
 
     // online
     var $_document_root = '/home/whitesummerbali/lerocomputer.com';
-	var $_api_url = 'https://www.rest.thriftex.id/api/';
-    var $_site_url = 'https://www.thriftex.id/';
+	var $_api_url = 'https://rest.thriftex.id/api/';
+    var $_site_url = 'https://thriftex.id/';
+    var $_app_mode = 'production';
+    // --- mode
+    // *     development
+    // *     testing
+    // *     production
 
     // offlien
     // var $_document_root = '/Applications/XAMPP/xamppfiles/htdocs/my/thriftex/';
 	// var $_api_url = 'http://localhost:8000/my/ci3api/api/';
     // var $_site_url = 'http://localhost:8000/my/thriftex/';
+    // var $_app_mode = 'development';
 
 	var $_app_version = '1.0.1';
 	// X versi Mayor (Perubahan arsitektur/fitur besar besaran)
