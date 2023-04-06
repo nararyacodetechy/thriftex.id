d8c6e98c53670b9bfb52eb544f7faef3558f0cc0
Gede Sugandi <gedesugandi@gmail.com>
2023-04-06 17:17:26 +0800

    update config

--- configuration.php
+++ configuration.php
@@ -4,16 +4,16 @@ class SConfig
 {
 
     // online
-    // var $_document_root = '/home/whitesummerbali/lerocomputer.com';
-	// var $_api_url = 'https://www.rest.thriftex.id/api/';
-    // var $_site_url = 'https://www.thriftex.id/';
+    var $_document_root = '/home/whitesummerbali/lerocomputer.com';
+	var $_api_url = 'https://www.rest.thriftex.id/api/';
+    var $_site_url = 'https://www.thriftex.id/';
 
     // offlien
-    var $_document_root = '/Applications/XAMPP/xamppfiles/htdocs/my/thriftex/';
-	var $_api_url = 'http://localhost:8000/my/ci3api/api/';
-    var $_site_url = 'http://localhost:8000/my/thriftex/';
+    // var $_document_root = '/Applications/XAMPP/xamppfiles/htdocs/my/thriftex/';
+	// var $_api_url = 'http://localhost:8000/my/ci3api/api/';
+    // var $_site_url = 'http://localhost:8000/my/thriftex/';
 
-	var $_app_version = '1.0.0';
+	var $_app_version = '1.0.1';
 	// X versi Mayor (Perubahan arsitektur/fitur besar besaran)
 	// Y versi Minor (Penambahan beberapa fitur )
 	// Z versi perbaikan/patch/bugfix. (perbaikan /bugfix,security fix dsb).
