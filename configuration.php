dbac91d809ef2c23c05d972a54acc0aac1a846a6
Bagus Nararya Nanda Raditya <87893990+nararyacodetechy@users.noreply.github.com>
2024-01-15 18:33:37 +0800

    Testing Configuration For Local Running

--- configuration.php
+++ configuration.php
@@ -1,37 +1,35 @@
-<?php 
+<?php
 
-class SConfig 
+class SConfig
 {
 
     // online
-    var $_document_root = '/home/whitesummerbali/lerocomputer.com';
-	var $_api_url = 'https://rest.thriftex.id/api/';
-    var $_site_url = 'https://thriftex.id/';
-    var $_app_mode = 'development';
-    var $_panel_qr = 'https://qr.thriftex.id/';
-    var $_domain = '.thriftex.id';
-    
+    // var $_document_root = '/home/whitesummerbali/lerocomputer.com';
+    // var $_api_url = 'https://rest.thriftex.id/api/';
+    // var $_site_url = 'https://thriftex.id/';
+    // var $_app_mode = 'development';
+    // var $_panel_qr = 'https://qr.thriftex.id/';
+    // var $_domain = '.thriftex.id';
+
     // --- mode
     // *     development
     // *     testing
     // *     production
 
     // offlien
-    // var $_document_root = '/Applications/XAMPP/xamppfiles/htdocs/my/thriftex/';
-	// var $_api_url = 'http://localhost:8000/my/ci3api/api/';
-    // var $_site_url = 'http://localhost:8000/my/thriftex/';
-    // var $_app_mode = 'development';
-    // var $_panel_qr = 'http://localhost:8000/my/thriftex_barcode/';
-    // var $_domain = 'localhost';
+    var $_document_root = 'C:/xampp/htdocs/verifex';
+    var $_api_url = 'http://localhost:8000/my/ci3api/api/';
+    var $_site_url = 'http://localhost/verifex/thriftex.id/';
+    var $_app_mode = 'development';
+    var $_panel_qr = 'http://localhost/verifex/thriftex_barcode/';
+    var $_domain = 'localhost';
 
     // 110276130330-7tiasjh4bcpmr34g490q497mp48cd1h7.apps.googleusercontent.com
     // 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
 
     // app version
-	var $_app_version = '1.2.10';
-	// X versi Mayor (Perubahan arsitektur/fitur besar besaran)
-	// Y versi Minor (Penambahan beberapa fitur )
-	// Z versi perbaikan/patch/bugfix. (perbaikan /bugfix,security fix dsb).
+    var $_app_version = '1.2.10';
+    // X versi Mayor (Perubahan arsitektur/fitur besar besaran)
+    // Y versi Minor (Penambahan beberapa fitur )
+    // Z versi perbaikan/patch/bugfix. (perbaikan /bugfix,security fix dsb).
 }
-
-?>
